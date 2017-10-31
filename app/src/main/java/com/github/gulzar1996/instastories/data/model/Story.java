1a3434f0cc6276547c81f786929b93a2e683371a
Gulzar Ahmed <gulzarhome54@gmail.com>
2017-10-31 14:51:14 +0530

    Story model added

---
+++ app/src/main/java/com/github/gulzar1996/instastories/data/model/Story.java
@@ -0,0 +1,15 @@
+package com.github.gulzar1996.instastories.data.model;
+
+import java.util.List;
+
+/**
+ * Created by gulza on 10/31/2017.
+ */
+
+public class Story {
+    public String id;
+    public String userName;
+    public String userAvatar;
+    public boolean seen;
+    public List<StoryList> list;
+}
---
+++ app/src/main/java/com/github/gulzar1996/instastories/data/model/StoryList.java
@@ -0,0 +1,17 @@
+package com.github.gulzar1996.instastories.data.model;
+
+/**
+ * Created by gulza on 10/31/2017.
+ */
+
+public class StoryList {
+    public String id;
+    public boolean isVideo;
+    public boolean seen;
+    public int duration;
+    public String src;
+    public String preview;
+    public String link;
+    public String linkText;
+    public long timestamp;
+}
