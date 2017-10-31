adc2dc44e9d166f35936ca45f442ea83d7fb001c
Gulzar Ahmed <gulzarhome54@gmail.com>
2017-10-31 15:25:29 +0530

    Status Activity added

---
+++ app/src/main/java/com/github/gulzar1996/instastories/ui/StatusActivty.java
@@ -0,0 +1,19 @@
+package com.github.gulzar1996.instastories.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.gulzar1996.instastories.R;
+
+/**
+ * Created by gulza on 10/31/2017.
+ */
+
+public class StatusActivty extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+    }
+}
